77335d096ee8f3fdcfa9cb42a4fceb73409938fc
espuleta0@gmail.com <espuleta0@gmail.com@ac962e5b-84ef-d8cd-1b1e-d8060f354428>
2012-05-08 15:22:01 +0000

    Parte da Documentação

--- trunk/Leilao/src/java/leilao/ProdutoLeilao.java
+++ trunk/Leilao/src/java/leilao/ProdutoLeilao.java
@@ -5,7 +5,7 @@
 package leilao;
 
 /**
- *
+ *  Produto a ser leiloado
  * @author Geovane Ferreira
  * @author Tiago 
  * 
@@ -15,7 +15,14 @@ public class ProdutoLeilao
 {
     private String nome;
     private int codigo,precoInicial,precoAtual,tempoTerminoLeilao;
-    
+    /**
+     * 
+     * @param codigo codigo do produto
+     * @param precoInicial preco inicial de negociacao
+     * @param precoAtual preco atual do produto
+     * @param nome  nome do produto
+     * @param tempoTerminoLeilao  tempo para o termino do leilao
+     */
     public ProdutoLeilao(int codigo, int precoInicial, int precoAtual,String nome, int tempoTerminoLeilao)
     {
         this.codigo=codigo;
@@ -24,37 +31,58 @@ public class ProdutoLeilao
         this.nome=nome;
         this.tempoTerminoLeilao=tempoTerminoLeilao;
     }
-    
+    /**
+     * 
+     * @return codigo do produto 
+     */
     public int getCodigo()
     {
         return codigo;
     }
-    
+    /**
+     * 
+     * @return preco inicial do produto 
+     */
     public int getPrecoInicial()
     {
         return precoInicial;
     }
-    
+    /**
+     * 
+     * @return preco atual do produto 
+     */
     public int getPrecoAtual()
     {
         return precoAtual;
     }
-    
+    /**
+     * 
+     * @return tempo de execucao do leilao 
+     */
     public int getTempoTerminoLeilao()
     {
         return tempoTerminoLeilao;
     }
-    
+    /**
+     * 
+     * @return nome do produto 
+     */
     public String getNome()
     {
         return nome;
     }
-
+    /**
+     * 
+     * @param precoAtual seta o valor do preco atual
+     */
     public void setPrecoAtual(int precoAtual)
     {
         this.precoAtual=precoAtual;
     }
-    
+   /**
+    * 
+    * @param temp seta o tempo do termino do leilao
+    */  
     public void setTempoTerminoLeilao(int temp)
     {
         this.tempoTerminoLeilao=temp;

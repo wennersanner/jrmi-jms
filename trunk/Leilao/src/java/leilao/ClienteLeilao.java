2348c67a466cf624455660650d7dd4836d163579
BateraOproprio@gmail.com <BateraOproprio@gmail.com@ac962e5b-84ef-d8cd-1b1e-d8060f354428>
2012-05-08 02:42:22 +0000

    Classes criadas agora só testar em pc separado

--- trunk/Leilao/src/java/leilao/ClienteLeilao.java
+++ trunk/Leilao/src/java/leilao/ClienteLeilao.java
@@ -18,7 +18,7 @@ import javax.naming.NamingException;
  *
  * @author tiago
  */
-public class ClienteLeilao implements MessageListener {
+public class ClienteLeilao {
 
     private String factoryName = "ConnectionFactory";
     private String topicName = "topic/LeilaoStatus";
@@ -27,47 +27,20 @@ public class ClienteLeilao implements MessageListener {
     private InterfaceLeiloeiro interfaceleiloeiro;
        
     public ClienteLeilao(meuRegistry meuRegistry) throws NamingException, JMSException {
-        Context jndiContext = new InitialContext();
-        TopicConnectionFactory factory = (TopicConnectionFactory) jndiContext.lookup(factoryName);
-        Topic topic = (Topic) jndiContext.lookup(topicName);
-        this.connect = factory.createTopicConnection();
-        TopicSession session = connect.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
-        TopicSubscriber subscriber = session.createSubscriber(topic);
-        subscriber.setMessageListener(this);
-        connect.start();
         this.meuRegistry=meuRegistry;
     }
 
-    @Override
-    public void onMessage(Message message) {
-        TextMessage textMsg = (TextMessage) message;
-        String text;
-        try {
-            text = textMsg.getText();
-            System.out.println(text);
-            String pesquisa = "Mouse";
-            if (text.matches(".*" + pesquisa + ".*")) {
-                try 
-                {
-                    interfaceleiloeiro=(InterfaceLeiloeiro) meuRegistry.getRegistry().lookup("001");
-                }
-                catch (RemoteException ex) {
-                    ex.printStackTrace();
-                } catch (NotBoundException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        } catch (JMSException ex) {
-            ex.printStackTrace();
-        }
-    }
     
 public void darNovoLance(int lance)
 {
+        
         try {
+            interfaceleiloeiro=(InterfaceLeiloeiro) meuRegistry.getRegistry().lookup("001");
             interfaceleiloeiro.darLance(lance);
         } catch (RemoteException ex) {
             Logger.getLogger(ClienteLeilao.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        }  catch (NotBoundException ex) {
+                    ex.printStackTrace();
+                }
 }
 }
--- trunk/Leilao/src/java/leilao/darLanceleilaoThread.java
+++ trunk/Leilao/src/java/leilao/darLanceleilaoThread.java
@@ -7,20 +7,44 @@ package leilao;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
 import javax.naming.NamingException;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 /**
  *
  * @author tiago
  */
-public class darLanceleilaoThread extends Thread implements Runnable {
+public class darLanceleilaoThread extends Thread implements Runnable,MessageListener {
 
     private JTextField jTextField1;
+    private JTextArea jTextArea;
+    private String factoryName = "ConnectionFactory";
+    private String topicName = "topic/LeilaoStatus";
+    private TopicConnection connect;
     
-    public darLanceleilaoThread(JTextField jTextField1)
+    public darLanceleilaoThread(JTextField jTextField1,JTextArea jTextArea) throws NamingException, JMSException
     {
         this.jTextField1=jTextField1;
+         Context jndiContext = new InitialContext();
+        TopicConnectionFactory factory = (TopicConnectionFactory) jndiContext.lookup(factoryName);
+        Topic topic = (Topic) jndiContext.lookup(topicName);
+        this.connect = factory.createTopicConnection();
+        TopicSession session = connect.createTopicSession(false, Session.AUTO_ACKNOWLEDGE);
+        TopicSubscriber subscriber = session.createSubscriber(topic);
+        subscriber.setMessageListener(this);
+        connect.start();
+        this.jTextArea=jTextArea;
     }
     
     @Override
@@ -39,7 +63,20 @@ public class darLanceleilaoThread extends Thread implements Runnable {
         } catch (JMSException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+       
 
     }
+
+    @Override
+    public void onMessage(Message message) {
+        TextMessage textMsg = (TextMessage) message;
+        String text;
+        try {
+            text = textMsg.getText();
+            jTextArea.append(text+"\n");
+        } catch (JMSException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }

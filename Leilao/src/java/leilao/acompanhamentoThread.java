16b50d79b62ac7423cf97058b70cebf55b5b0e1f
espuleta0 <espuleta0@gmail.com>
2012-05-08 15:18:36 +0000

    Parte da Documentação

--- Leilao/src/java/leilao/acompanhamentoThread.java
+++ Leilao/src/java/leilao/acompanhamentoThread.java
@@ -15,18 +15,26 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
- *
+ * Theard responsavel da parte do leiloeiro;
  * @author Geovane Ferreira
  * @author Tiago 
  * 
  * 
  */
+
 public class acompanhamentoThread extends Thread implements Runnable {
 
     private JTextArea jTextArea1;
     private JTextField jTextField1, jTextField2, jTextField3,edt_CodProd;
     private boolean continuaLeilao=true;
-
+    /**
+     * Dados necessarios para iniciar o leilao
+     * @param jTextArea1  Caixa de acompanhamento do leilão com os lances dados
+     * @param jTextField1 Valor Inicial
+     * @param jTextField2 Nome do Produto
+     * @param jTextField3 Tempo de duracao
+     * @param edt_CodProd Codigo produto
+     */
     public acompanhamentoThread(JTextArea jTextArea1, JTextField jTextField1, JTextField jTextField2, JTextField jTextField3, JTextField edt_CodProd) {
         this.jTextArea1 = jTextArea1;
         this.jTextField1 = jTextField1;
@@ -36,6 +44,11 @@ public class acompanhamentoThread extends Thread implements Runnable {
     }
 
     @Override
+    /**
+     * Responsavel por criar o leilao atraves de leiloeiro;
+     * Verifica o tempo do leilao; 
+     * Publicar o fim do leilao;
+     */
     public void run() {
         meuRegistry mRegistry = new meuRegistry();
         ProdutoLeilao pl = new ProdutoLeilao(1, Integer.parseInt(jTextField1.getText()), Integer.parseInt(jTextField1.getText()), jTextField2.getText(), Integer.parseInt(jTextField3.getText()));
@@ -96,7 +109,10 @@ public class acompanhamentoThread extends Thread implements Runnable {
             Logger.getLogger(Form_Leiloeiro3.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+    /**
+     * Responsavel por parar ou nao o leilao
+     * @param aux 
+     */
     public void setContinuaLeilao(boolean aux)
     {
         this.continuaLeilao=aux;
